c5ca92055377a47a5c363dd1005ee3cd8e112cfe
FoniksFox <borisbeslimov@gmail.com>
2026-04-08 17:11:10 +0000

    fix: Default to disabled spi errors and timeout

--- Core/Inc/Common/Flags.hpp
+++ Core/Inc/Common/Flags.hpp
@@ -4,7 +4,7 @@
 // ============================================
 // SPI Communication Flags
 // ============================================
-constexpr bool ENABLE_SPI_ERROR_HANDLING = true;
-constexpr bool ENABLE_SPI_TIMEOUT = true;
+constexpr bool ENABLE_SPI_ERROR_HANDLING = false;
+constexpr bool ENABLE_SPI_TIMEOUT = false;
 
 #endif // FLAGS_HPP
